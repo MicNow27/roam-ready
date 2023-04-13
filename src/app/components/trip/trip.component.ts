6eb0725f35377321f6702e6d96ffedb8f6bedbf5
michelle <michellenowers@gmail.com>
2023-04-13 21:29:24 +0200

    feat: Trip component routing

--- src/app/components/trip/trip.component.ts
+++ src/app/components/trip/trip.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Trip } from '../../models/user.data';
+import { TripsService } from '../../services/trips/trips.service';
 
 @Component({
   selector: 'app-trip',
   templateUrl: './trip.component.html',
   styleUrls: ['./trip.component.scss'],
 })
-export class TripComponent {
-  @Input() trip: Trip | undefined;
-  @Input() routeName = '';
+export class TripComponent implements OnInit {
+  trip: Trip | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private tripsService: TripsService
+  ) {}
+
+  ngOnInit() {
+    const tripName = this.route.snapshot.queryParamMap.get('tripName');
+    if (tripName) this.trip = this.tripsService.getTrip(tripName);
+  }
 }
