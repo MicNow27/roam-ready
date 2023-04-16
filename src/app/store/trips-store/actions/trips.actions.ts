9bb5ae96ff301f1e8d047d791f2c814c9c596788
michelle <michellenowers@gmail.com>
2023-04-17 00:22:29 +0200

    feat: Selectors and actions

--- src/app/store/trips-store/actions/trips.actions.ts
+++ src/app/store/trips-store/actions/trips.actions.ts
@@ -1,15 +1,69 @@
 import { createAction, props } from '@ngrx/store';
+import { Activity, Trip } from '../../../models/user.data';
 
-export const tRIPSTripss = createAction(
-  '[Trips] TRIPS Tripss'
+export const addTrip = createAction(
+  '[Trips] TRIPS Add Trip',
+  props<{ trip: Trip }>()
 );
 
-export const tRIPSTripssSuccess = createAction(
-  '[Trips] TRIPS Tripss Success',
-  props<{ data: any }>()
+export const updateTrip = createAction(
+  '[Trips] TRIPS Update Trip',
+  props<{ oldTrip: Trip; newTrip: Trip }>()
 );
 
-export const tRIPSTripssFailure = createAction(
-  '[Trips] TRIPS Tripss Failure',
-  props<{ error: any }>()
+export const deleteTrip = createAction(
+  '[Trips] TRIPS Delete Trip',
+  props<{ trip: Trip }>()
+);
+
+export const loadTrip = createAction(
+  '[Trips] TRIPS Load Trip',
+  props<{ tripName: string }>()
+);
+
+export const loadTrips = createAction('[Trips] TRIPS Load Trips');
+
+export const loadTripsSuccess = createAction(
+  '[Trips] TRIPS Load Trips Success',
+  props<{ trips: Trip[] }>()
+);
+
+export const loadTripsFailure = createAction(
+  '[Trips] TRIPS Load Trips Failure',
+  props<{ error: string }>()
+);
+
+export const addActivity = createAction(
+  '[Trips] TRIPS Add Activity',
+  props<{ activity: Activity }>()
+);
+
+export const updateActivity = createAction(
+  '[Trips] TRIPS Update Activity',
+  props<{ newActivity: Activity }>()
+);
+
+export const deleteActivity = createAction(
+  '[Trips] TRIPS Delete Activity',
+  props<{ activity: Activity }>()
+);
+
+export const loadActivity = createAction(
+  '[Trips] TRIPS Load Activity',
+  props<{ tripName: string; activityName: string }>()
+);
+
+export const loadActivities = createAction(
+  '[Trips] TRIPS Load Activities',
+  props<{ tripName: string }>()
+);
+
+export const loadActivitiesSuccess = createAction(
+  '[Trips] TRIPS Load Activities Success',
+  props<{ activities: Trip[] }>()
+);
+
+export const loadActivitiesFailure = createAction(
+  '[Trips] TRIPS Load Activities Failure',
+  props<{ error: string }>()
 );
