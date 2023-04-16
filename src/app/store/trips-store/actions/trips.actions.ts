90b3b9c8b0698ad98fc50a5a1781f2cbaacf761d
michelle <michellenowers@gmail.com>
2023-04-17 00:54:12 +0200

    feat: Trips actions update

--- src/app/store/trips-store/actions/trips.actions.ts
+++ src/app/store/trips-store/actions/trips.actions.ts
@@ -1,6 +1,7 @@
-import { createAction, props } from '@ngrx/store';
-import { Activity, Trip } from '../../../models/user.data';
+import {createAction, props} from '@ngrx/store';
+import {Activity, Trip} from '../../../models/user.data';
 
+// Trip CUD
 export const addTrip = createAction(
   '[Trips] TRIPS Add Trip',
   props<{ trip: Trip }>()
@@ -16,11 +17,23 @@ export const deleteTrip = createAction(
   props<{ trip: Trip }>()
 );
 
+// R - 1 trip
 export const loadTrip = createAction(
   '[Trips] TRIPS Load Trip',
   props<{ tripName: string }>()
 );
 
+export const loadTripSuccess = createAction(
+  '[Trips] TRIPS Load Trip Success',
+  props<{ trip: Trip }>()
+);
+
+export const loadTripFailure = createAction(
+  '[Trips] TRIPS Load Trip Failure',
+  props<{ error: string }>()
+);
+
+// R - all trips
 export const loadTrips = createAction('[Trips] TRIPS Load Trips');
 
 export const loadTripsSuccess = createAction(
@@ -33,6 +46,7 @@ export const loadTripsFailure = createAction(
   props<{ error: string }>()
 );
 
+// Activity CUD
 export const addActivity = createAction(
   '[Trips] TRIPS Add Activity',
   props<{ activity: Activity }>()
@@ -48,11 +62,23 @@ export const deleteActivity = createAction(
   props<{ activity: Activity }>()
 );
 
+// R - 1 activity
 export const loadActivity = createAction(
   '[Trips] TRIPS Load Activity',
   props<{ tripName: string; activityName: string }>()
 );
 
+export const loadActivitySuccess = createAction(
+  '[Trips] TRIPS Load Activity Success',
+  props<{ activity: Activity }>()
+);
+
+export const loadActivityFailure = createAction(
+  '[Trips] TRIPS Load Activity Failure',
+  props<{ error: string }>()
+);
+
+// R - all activities
 export const loadActivities = createAction(
   '[Trips] TRIPS Load Activities',
   props<{ tripName: string }>()
